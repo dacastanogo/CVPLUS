17aa95177e1c68c32245c7f8ed3bb2e023431a07
dacastanogo <dcastanogomez@gmail.com>
2020-03-18 13:07:32 -0500

    Added MongoDB Atlas connection

--- server/src/database.js
+++ server/src/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-const URI = process.env.MONGODB_URI ?
-    process.env.MONGODB_URI
-    : 'mongodb://localhost/databasetest';
+const URI = "mongodb+srv://admin:santiagoesmeragueva@cvplus-iatmc.mongodb.net/test?retryWrites=true&w=majority"
+    //process.env.MONGODB_URI ?
+    //process.env.MONGODB_URI
+    //: 'mongodb://localhost/databasetest';
 
 mongoose.connect(URI, {
     useNewUrlParser: true,
